cc5e9b55e0afef3c24d46387dda2f37728a38e3b
Darrion Gering <darrionkg@gmail.com>
2019-03-25 22:51:49 -0700

    Add ready and more click functions

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,17 @@ jQuery("p").click(function() {
 jQuery("img").click(function() {
   alert("This is an image.");
 });
+
+$(document).ready(function() {
+  $("h1").click(function() {
+    alert("This is a header.");
+  });
+
+  $("p").click(function() {
+    alert("This is a paragraph.");
+  });
+
+  $("img").click(function() {
+    alert("This is an image.");
+  });
+});
